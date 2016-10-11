f1e132fd61875406585b6a966478f623260aa5e8
Aws A <pisceswolf96@gmail.com>
2016-10-12 02:08:56 +0300

    fix some typos

--- tabannusi.c
+++ tabannusi.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
  /* If the pid is less than zero, something went wrong when forking */
  if (pid < 0) 
 {
-/* Now we will tel the  log server  cant fork*/
+/* Now we will tell the log server can't fork*/
  syslog(LOG_NOTICE, "Cant Fork\n");
  exit(EXIT_FAILURE);
  }
@@ -29,7 +29,7 @@ if (pid > 0)
  /* If execution reaches this point we are the child */
 /* Creating a Unique Session ID (SID)*/
     if (setsid() < 0)
- syslog(LOG_NOTICE, "I cant Creat a Unique Session ID ");
+ syslog(LOG_NOTICE, "I can't create a Unique Session ID ");
         exit(EXIT_FAILURE);
     //catch/ignore signals
     signal(SIGCHLD,SIG_IGN);
@@ -40,8 +40,8 @@ pid_t pid1;
  /* If the pid1 is less than zero, something went wrong when forking */
  if (pid1 < 0) 
 {
-/* Now we will tel the  log server  cant fork*/
- syslog(LOG_NOTICE, "Cant Fork\n");
+/* Now we will tell the log server can't fork*/
+ syslog(LOG_NOTICE, "Can't fork\n");
  exit(EXIT_FAILURE);
  }
  /* If the pid1 we got back was greater than zero, then the clone was successful and we are the parent. */ 
@@ -49,16 +49,16 @@ if (pid1 > 0)
  {
  exit(EXIT_SUCCESS);
  }
- /* Now we will Set the umask to zero */ 
+ /* Now we will set the umask to zero */ 
 umask(0);
-/* its seme ok now*/
+/* it's the same ok now*/
  /* Now we will Change the current working directory to root */
  chdir("/");
 /* Close all Files */
  close(STDIN_FILENO);
  close(STDOUT_FILENO);
  close(STDERR_FILENO);
-/* Now we will tel the log server its ok now */
+/* Now we will tell the log server it's ok now */
  syslog(LOG_NOTICE, "Successfully started daemon\n");
  /* close a connection to the syslog server */
  closelog();
