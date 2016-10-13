ef2525735595ab43d07d6a19b7fd0ac576c6806a
alimiracle <blade.vp2020@gmail.com>
2016-10-13 15:48:59 -0700

    make build.sh mor good

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@ if echo "$list" | grep -q "Cargo.toml"; then
  cargo build
 exit
 fi
+if echo "$list" | grep -q "autogen.sh"; then
+chmod +x autogen.sh
+./autogen.sh
+fi
 if echo "$list" | grep -q "configure"; then
 chmod +x configure
+./configure
 fi
 make
